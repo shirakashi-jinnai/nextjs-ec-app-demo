0b4cccab888c2ae6739755d3e016e91c0d3d1b2e
shirakashi-jinnai <j.shirakashi.jb@gmail.com>
2021-10-27 14:48:34 +0900

    プロダクト一覧を作成

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,18 @@
-import React from "react";
-import Head from "next/head";
+import React from 'react'
+import Head from 'next/head'
+import NextLink from 'next/link'
 import {
   AppBar,
   Container,
   IconButton,
+  Link,
   Toolbar,
   Typography,
-} from "@mui/material";
-import useStyles from "../utils/styles";
+} from '@mui/material'
+import useStyles from '../utils/styles'
 
 const Layout = ({ children }) => {
-  const classes = useStyles();
+  const classes = useStyles()
   return (
     <div>
       <Head>
@@ -19,7 +21,20 @@ const Layout = ({ children }) => {
       </Head>
       <AppBar position="static" className={classes.navBar}>
         <Toolbar>
-          <Typography>Amazona</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Amazona</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
@@ -27,7 +42,7 @@ const Layout = ({ children }) => {
         <Typography>All rights reserved. Next Amazona</Typography>
       </footer>
     </div>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
+import _ from "lodash";
+import { Grid, CardActionArea, Typography } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+import data from "../utils/data";
+
 const Home = () => {
   return (
     <div>
       <h1>Product</h1>
+      <Grid container spacing={3}>
+        {_.map(data.products, (product, key) => (
+          <Grid item md={4} key={product.name}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  image={product.image}
+                  title={product.name}
+                />
+                <CardContent>
+                  <Typography>{product.name}</Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Typography>${product.price}</Typography>
+                <Button size="small">Add to cart</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
